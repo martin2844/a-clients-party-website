ef1af184692fe5ac1cf186426a90dd5361172fa5
unknown <martin2844@gmail.com>
2020-02-09 17:08:37 -0300

    added service extras

--- src/pages/servicio.js
+++ src/pages/servicio.js
@@ -3,39 +3,76 @@ import Layout from '../layout/Layout';
 import TipiPack from '../components/TipiPack';
 import '../stylesheets/servicio.scss';
 
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+
 const Servicio = () => {
+
+    const star = useStaticQuery(graphql`
+    query {
+        icons: allFile(sort: {fields: [name], order: ASC}, filter: { sourceInstanceName: { eq: "icons" } }) {
+           edges {
+             node {
+               childImageSharp {
+                 fluid(quality: 100, maxWidth: 200) {
+                     ...GatsbyImageSharpFluid
+                     originalName
+                 }
+               }
+               
+             }
+           }
+         }
+        }
+    
+    `);
+    
+        console.log(star);
+
+    let starImageSrc = star.icons.edges.filter((node) => {
+        return node.node.childImageSharp.fluid.originalName === "star.png";
+    })
+    let starImage = starImageSrc[0].node.childImageSharp.fluid;
+
+    
 
 
     const extraInfo = [
         {
             title: "Bandeja de desayuno",
             desc: "Incluye bandeja, vaso con tapa o botella, plato, servilleta, cupcake, jugo de naranja o chocolatada",
-            price: 210
+            price: 210,
+            color: "green"
         },
         {
             title: "Souvenir para invitados",
             desc: "Es personalizado, se consultan los gustos y preferencias, y se piensan propuestas",
-            price: 150
+            price: 150,
+            color: "salmon"
         },
         {
             title: "Snacks para la noche",
             desc: "Opción de bolsas de pochoclos, cono de snacks salados, bolsitas de golosinas",
-            price: 150
+            price: 150,
+            color: "pink"
         },
         {
             title: "Set de Higiene",
             desc: "Incluye pasta de dientes, cepillo de dientes y peine",
-            price: 150
+            price: 150,
+            color: "green"
         },
         {
             title: "Ambientación con globos de hielo",
             desc: "Opción de elegir colores",
-            price: "Consultanos precios"
+            price: "Consultanos precios",
+            color: "salmon"
         },
         {
             title: "Baul sorpresa",
             desc: "Incluye cotillon para jugar y sacarse fotos",
-            price: 150
+            price: 150,
+            color: "green"
         }
     ]
 
@@ -43,14 +80,14 @@ const Servicio = () => {
     let mapExtraInfo = extraInfo.map((item) => {
         return (
             <div className="info-iteree">
-            <div className="star-container">
-                
-            </div>
-            <div className="info-container">
-                 <h3>{item.title}</h3>
-                 <p>{item.desc}</p>
-                 <p>{typeof item.price === "string" ? item.price : ("$" + item.price + " x persona")}</p>
-            </div>
+                 <div className="star-container">
+                     <Img className="star" fluid={starImage} />
+                 </div>
+                 <div className="info-container">
+                      <h3 className={`title-${item.color}`}>{item.title}</h3>
+                         <p>({item.desc})</p>
+                     <p className="pack-price">{typeof item.price === "string" ? item.price : ("$" + item.price + " x persona")}</p>
+                 </div>
             </div>
         )
     })
@@ -65,12 +102,14 @@ const Servicio = () => {
                         <TipiPack packType="magic" />
                         <TipiPack packType="premium" />
                     </div>
+                    <div className="center-wrap">
                     <div className="pack-extras-container"> 
                         <div className="pack-title">
                             <h3>A cualquiera de estos packs podes agregarle estos items aparte</h3>
                         </div>
                         {mapExtraInfo}
 
+                    </div>
                     </div>
         </Layout>
     )
